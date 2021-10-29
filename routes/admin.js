fed1a7160c4aab2f70aa20565219b22415728183
steninxavier <steninxavier@gmail.com>
2021-10-30 00:16:32 +0200

    form created for the product creation

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   let products = [
     {
@@ -32,5 +32,11 @@ router.get('/', function(req, res, next) {
 
   res.render('admin/view-products',{admin: true, products})
 });
-
+router.get('/add-product', function(req, res) {
+  res.render('admin/add-product')
+});
+router.post('/add-product',(req,res)=>{
+  console.log(req.body);
+  console.log(req.files.Image)
+});
 module.exports = router;
